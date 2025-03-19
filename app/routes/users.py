48bd54b5f9b235e1674353c41c3f7833b460bd1b
anggreinipra <anggreinipra@gmail.com>
2025-03-19 16:19:49 +0700

    update models

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,7 +1,7 @@
 import logging
 from flask import Blueprint, request, jsonify
 from app.models import (
-    get_all_users, get_user_by_id, add_user, update_user_profile, delete_user
+    get_all_users, get_user_by_email, get_user_by_id, add_user, update_user_profile, delete_user
 )
 
 users_bp = Blueprint("users", __name__)
